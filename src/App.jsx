0dd9053b28ccfd7cf40dd5b545323e97c81798d2
Oleksii Khalin EL <oleksii.khalin@elektro-ljubljana.si>
2026-09-09 15:41:28 +0200

    Making first lines

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Result from './components/Result'
+import UserInput from './components/UserInput'
+
+function App() {
+  return (
+    <body>
+      <Header />
+      <h1>React Investment Calculator</h1>
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import headerImg from '../assets/investment-calculator-logo.png'
+export default function Header() {
+    return(
+        <div id='header'>
+            <img src={ headerImg } alt='big sack with money' />
+        </div>
+    );
+}
